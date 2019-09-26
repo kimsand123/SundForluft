dad4f89fe2b1beacfd3458ec5a67570ee87cb04b
Kim Sandberg <s@ndberg.dk>
2019-09-26 15:25:15 +0200

    trying to make button 1/3 of screensize programmatically

--- SundForluft/ViewController.swift
+++ SundForluft/ViewController.swift
@@ -11,8 +11,19 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var logButton: UIBarButtonItem!
+    
+    @IBOutlet weak var ChallengeButton: UIButton!
+    
+    @IBOutlet weak var ppmLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        let screenRect = UIScreen.main.bounds
+        let screenWidth = screenRect.size.width
+        let screenHeight = screenRect.size.height
+        ppmLabel.text = String(Int(screenHeight))
+        //ChallengeButton.heightAnchor = screenHeight / 4
         // Do any additional setup after loading the view.
     }
 
