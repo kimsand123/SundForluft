d4df9323fe07add4c39f2583d3d0314c6cca164b
Konstantin <stenilo@gmail.com>
2019-10-03 13:50:15 +0200

    pedaltableviewadded

---
+++ SundForluft/PedalDataTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PedalDataTableViewController.swift
+//  SundForluft
+//
+//  Created by Konstantin Orlov on 03/10/2019.
+//  Copyright © 2019 Kim Sandberg. All rights reserved.
+//
+
+import UIKit
+
+class PedalDataTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
