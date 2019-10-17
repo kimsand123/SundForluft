b45326d9a75d0f6ed568fcd2cccae6aa24b2e7dc
Kim Sandberg <s@ndberg.dk>
2019-10-17 14:41:59 +0200

    Beginning to make the chartspage. now gotta try the podthing

---
+++ SundForluft/Views/RoomDetailedViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RoomDetailedViewController.swift
+//  SundForluft
+//
+//  Created by Kim Sandberg on 17/10/2019.
+//  Copyright © 2019 Kim Sandberg. All rights reserved.
+//
+
+import UIKit
+
+class RoomDetailedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
